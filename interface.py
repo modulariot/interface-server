4428dac1e0f440af091d803a5c0a63e884b6a235
Andrew <drewd8@vt.edu>
2017-01-20 18:44:08 -0500

    add a class for iot devices

--- interface.py
+++ interface.py
@@ -8,5 +8,24 @@ app = Flask(__name__)
 def index():
     return 'Hello, world!'
 
+class Device(object):
+    'A IOT device.'
+    def __init__(self, id_number, device_type, state):
+        self.id_number = id_number
+        self.device_type = device_type
+        self.state = state
+    def id(self):
+        'The unique ID number of the device.'
+        return self.id_number
+    def type(self):
+        'The type of device.'
+        return self.device_type
+    def state(self):
+        'The current state of the device.'
+        return state
+    def update(self, new_state):
+        'Update the state of the device.'
+        self.state = new_state
+
 if __name__ == '__main__':
     app.run()
